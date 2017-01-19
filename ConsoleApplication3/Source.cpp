4e7202dbde26afa43bb34f2a5cb10e342e43e622
Aftermathx25 <aftermathx25@o2.pl>
2017-01-19 17:45:00 +0100

    Initial commit

---
+++ ConsoleApplication3/Source.cpp
@@ -0,0 +1,15 @@
+#include <omp.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+#include <stdio.h>
+#include <time.h>
+
+
+int main(int argc, char* argv[])
+{
+	system("pause");
+	return 0;
+}
---
+++ ConsoleApplication3/TestApp.cpp
@@ -0,0 +1,52 @@
+#include <omp.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+#include <stdio.h>
+#include <time.h>
+
+long long num_steps = 100000000;
+double step;
+
+int main(int argc, char* argv[])
+{
+	omp_set_num_threads(86);
+	clock_t start, stop;
+	double x, pi, sum = 0.0;
+	int i;
+	step = 1. / (double)num_steps;
+	start = clock();
+	double suma1 = 0.0;
+#pragma omp parallel shared (sum) private (suma1)
+	{
+		suma1 = 0.0;
+#pragma omp for
+		for (i = 0; i < num_steps; i++)
+		{
+			x = (i + .5)*step;
+			suma1 += 4.0 / (1. + x*x);
+
+		}
+#pragma omp atomic
+		sum += suma1;
+	}
+	pi = sum*step;
+	stop = clock();
+
+	FILE* plik;
+	fopen_s(&plik, "wyniki4.csv", "a");
+	if (plik == NULL) printf("Coœ posz³o nie tak.\n");
+	else
+	{
+
+	}
+
+	printf("Wartosc liczby PI wynosi %15.12f\n", pi);
+	fprintf(plik, "%f;%d\n", ((double)(stop - start) / 1000.0), num_steps);
+
+	fclose(plik);
+	system("pause");
+	return 0;
+}
